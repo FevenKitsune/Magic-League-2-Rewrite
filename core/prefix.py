1a5da1145b70a6ed2784179ac98a6f856171cbcf
Will S <fevenkitsune@gmail.com>
2021-11-11 21:35:02 -0500

    + format improvements

--- core/prefix.py
+++ core/prefix.py
@@ -3,6 +3,7 @@ from db.defaults import defaults
 from db.get import getConfig
 from db.set import setConfig
 
+
 async def get_prefix(bot, message):
     """Checks if the bot has a configuration tag for the prefix. Otherwise, gets the default."""
     if isinstance(message.channel, discord.DMChannel):
